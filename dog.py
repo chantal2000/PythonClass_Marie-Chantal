737a6275a8fcf7fa4c01996e6d4937b3dbb2e41f
chantal2000 <79141873+chantal2000@users.noreply.github.com>
2021-05-24 20:04:35 +0300

    Update dog.py

--- dog.py
+++ dog.py
@@ -1,10 +1,11 @@
+
 class Dog:
     def __init__(self,name,weight):
         self.name=name
         self.weight=weight
     def eat(self):
-        return self.name + "has" + self.weight
+        return f"{self.name} has {self.weight}"
     def serve_people(self):
-        return  self.name + 'serves people'
+        return  f"{self.name}  serves people"
     def move(self):
-        return  self.name +" has moved " + "3kilometers" + "and has" + self.weight
+        return  f"{self.name} has moved 3kilometers and has {self.weight}"
